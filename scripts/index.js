00e89336b1e9d07272ca7d3d1e7b866fd14e3b42
Mikhail Metsler <metsler.ml@yandex.ru>
2021-09-13 00:17:13 +0300

    feat: доваить код JS

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let popup = document.querySelector(".popup");
+let popupOpen = document.querySelector(".profile__button-edit");
+let popupClose = popup.querySelector(".popup__btn-close");
+let saveForm = popup.querySelector(".popup__btn-save");
+
+function popupToggle() {
+  popup.classList.toggle("popup_opened");
+}
+// --- закрытие popup вне формы
+function clickOverlay(event) {
+  if (event.target === event.currentTarget) {
+    popupToggle();
+  }
+}
+popup.addEventListener("click", clickOverlay);
+//
+
+popupOpen.addEventListener("click", popupToggle);
+popupClose.addEventListener("click", popupToggle);
+saveForm.addEventListener("click", popupToggle);
+
+// Код добавления данных в профиль
+
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__name");
+let jobInput = formElement.querySelector(".popup__about-self");
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  let profJob = document.querySelector(".profile__about-self");
+  let profName = document.querySelector(".profile__name");
+
+  profName.textContent = nameInput.value;
+  profJob.textContent = jobInput.value;
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
