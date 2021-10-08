c1a2ea2802d93da065ce8512ff58ed84def64a0e
Mikhail Metsler <metsler.ml@yandex.ru>
2021-10-08 23:08:47 +0300

    feat: добавить стилизацию кнопок и полей

---
+++ scripts/validate.js
@@ -0,0 +1,9 @@
+enableValidation({
+  formSelector: '.popup__container',
+  inputSelector: '.popup__text',
+  submitButtonSelector: '.popup__btn-save',
+  inactiveButtonClass: 'popup__btn-save_disabled',
+  inputErrorClass: 'popup__text_type_error',
+  errorClass: 'popup__text-error_visible'
+});
+
